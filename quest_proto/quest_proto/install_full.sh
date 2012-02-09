aaf3d51637c0a72f9715da57194309f9e0aa7ef6
Mikko Johansson <mikko.johansson@aalto.fi>
2012-02-09 14:46:57 +0200

    First commit

---
+++ quest_proto/quest_proto/install_full.sh
@@ -0,0 +1,18 @@
+#django_geonition_utils
+easy_install https://github.com/geonition/django_geonition_utils/tarball/2.0.x
+#django_auth
+easy_install https://github.com/geonition/django_auth/tarball/2.0.x
+#django_email
+easy_install https://github.com/geonition/django_email/tarball/2.0.x
+#django_geojson_rest
+easy_install https://github.com/geonition/django_geojson_rest/tarball/2.0.x
+#django_user_profile (This is deprecated, install only if necessary)
+#easy_install https://github.com/geonition/django_user_profile/tarball/master
+#django_opensocial_people
+easy_install https://github.com/geonition/django_opensocial_people/tarball/2.0.x
+#django_opensocial_mediaitems
+#easy_install https://github.com/geonition/django_opensocial_mediaitems/tarball/master
+#django_geonition_client
+easy_install https://github.com/geonition/django_geonition_client/tarball/2.0.x
+#data_processing
+easy_install https://github.com/geonition/django_geojson_data_processing/tarball/1.0.x
---
+++ quest_proto/quest_proto/urls.py
@@ -0,0 +1,25 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'quest_proto.views.home', name='home'),
+    # url(r'^quest_proto/', include('quest_proto.foo.urls')),
+
+    #geonition urls
+    (r'^gclient/', include('geonition_client.urls')),
+    (r'^guser/', include('auth.urls')),
+    #(r'^gprofile/', include('user_profile.urls')),
+    (r'^gfeature/', include('geojson_rest.urls')),
+    (r'^gemail/', include('email_rest.urls')),
+    (r'^gopen_people/', include('opensocial_people.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
