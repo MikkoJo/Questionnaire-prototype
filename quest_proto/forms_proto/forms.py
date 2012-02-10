fc91564d6e74499f186075df9aa8a9cb7c545e84
Mikko Johansson <mikko.johansson@aalto.fi>
2012-02-10 14:55:24 +0200

    Created form class that creates CharFields dynamically

--- quest_proto/forms_proto/forms.py
+++ quest_proto/forms_proto/forms.py
@@ -2,5 +2,15 @@ from django import forms
 
 class questForm(forms.Form):
     name = forms.CharField(required=False, max_length=100)
-    
+    #print (kwargs)
+    def __init__(self, *args, **kwargs):
+        print (kwargs)
+        definition = kwargs.pop('definition')
+        print(definition)
+        super(questForm, self).__init__(*args, **kwargs)
+        for dyn_field in definition:
+            print(dyn_field['args'])
+            self.fields[dyn_field['name']] = forms.CharField(**dyn_field['args'])
+        
+        
     
